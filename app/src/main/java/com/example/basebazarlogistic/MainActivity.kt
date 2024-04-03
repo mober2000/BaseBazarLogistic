bba1b4e171b26e6857222d653ed6cac5cebb0885
mober2000 <chebik2012@gmail.com>
2024-04-03 22:12:26 +0300

    Добавил расчет плотности товара

--- app/src/main/java/com/example/basebazarlogistic/MainActivity.kt
+++ app/src/main/java/com/example/basebazarlogistic/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.basebazarlogistic
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -30,25 +33,44 @@ class MainActivity : AppCompatActivity() {
         var widthButton: EditText = findViewById(R.id.inputWidth)
         var heightButton: EditText = findViewById(R.id.inputHeight)
         var pressButton: Button = findViewById(R.id.calculateButton)
+        var inputWeight: EditText = findViewById(R.id.inputWeight)
+        var destinyText = findViewById<TextView>(R.id.destinySum)
 
         pressButton.setOnClickListener {
             val length = lengthButton.text.toString().toDoubleOrNull() ?: 0.0
             val width = widthButton.text.toString().toDoubleOrNull() ?: 0.0
             val height = heightButton.text.toString().toDoubleOrNull() ?: 0.0
-
-            Log.d("MainActivity", "Length: $length")
-            Log.d("MainActivity", "Width: $width")
-            Log.d("MainActivity", "Height: $height")
+            val weightOneBox = inputWeight.text.toString().toDoubleOrNull() ?: 0.0
 
             val volume = length * width * height
 
-            Log.d("MainActivity", "Volume: $volume")
-
-            // Форматируем результат до стотысячных
+            // Форматируем результат до стотысячных и считаем обьем
             val formattedVolume = String.format("%.5f", volume)
 
             volumeResultSum.text = formattedVolume
 
+            // Рассчитываем destiny (плотность)
+            val destiny = if (volume != 0.0) {
+                weightOneBox / volume
+            } else {
+                0.0 // Если объём равен 0, ставим плотность в 0
+            }
+
+            // Форматируем значение destiny до стотысячных и выводим
+            val formattedDestiny = String.format("%.5f", destiny)
+            destinyText.text = formattedDestiny
+
         }
+
+        // Получаем ссылку на Spinner по его id
+        val spinner: Spinner = findViewById(R.id.spinner)
+        // Массив строк для отображения в списке
+        val options = arrayOf("Хозтовары", "Одежда", "Электроника", "Poizon")
+        // Создаем адаптер
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        // Устанавливаем ресурс макета для выпадающего списка
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Устанавливаем адаптер для выпадающего списка
+        spinner.adapter = adapter
     }
 }
